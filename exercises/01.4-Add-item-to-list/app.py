7c7758e54f2db1ae9031c92dec8043b4ad1b7026
natigalsan <natividadgalsan@gmail.com>
2022-05-16 11:22:45 +0000

    ejercicio 1.4

--- exercises/01.4-Add-item-to-list/app.py
+++ exercises/01.4-Add-item-to-list/app.py
@@ -1,5 +1,21 @@
 #Remember import random function here:
-
-my_list = [4,5,734,43,45]
+import random
 
 #The magic is here:
+def generate_random_list():
+
+    my_list = [4,5,734,43,45]
+    
+    numeros_aleatorios=random.sample(range(100), 10)
+
+    for i in range(numeros_aleatorios):
+
+        my_list.append(numeros_aleatorios)
+        i += i
+        return my_list
+
+
+
+print(generate_random_list(my_list))
+
+
